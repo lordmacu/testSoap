a3c44cf23fed64666380876b4d4ee99008db736c
Cristian Garcia <cristian.garcia@datia.co>
2020-05-13 23:59:55 -0500

    add process routes

--- libs/Route.php
+++ libs/Route.php
@@ -6,14 +6,10 @@ class Route{
 		if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 			if(isset($_GET["action"])){
 				if($_GET["action"]==$route){
-					$controllerExplode=explode("@",$controller);
-					$controller= new $controllerExplode[0]();
-		    		return  $controller->{$controllerExplode[1]}();
+				return (new self)->processRoute($controller);
 				}
 			}else{
-				$controllerExplode=explode("@",$controller);
-				$controller= new $controllerExplode[0]();
-	    		return  $controller->{$controllerExplode[1]}();
+				return (new self)->processRoute($controller);
 			}
 	   		
 		}
@@ -22,11 +18,15 @@ class Route{
 	public static  function post($route,$controller){
 		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	   		if($_GET["action"]==$route){
-				$controllerExplode=explode("@",$controller);
-				$controller= new $controllerExplode[0]();
-	    		return  $controller->{$controllerExplode[1]}();
+				return (new self)->processRoute($controller);
 			}
 		}
+	}
+
+	public static function processRoute($controller){
+		$controllerExplode=explode("@",$controller);
+		$controller= new $controllerExplode[0]();
+		return  $controller->{$controllerExplode[1]}();
 	}
 
 }
